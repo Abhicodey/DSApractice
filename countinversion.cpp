d7d7e72386bd98422d0795f7fd5c961b361508ee
Abhicodey <the.ar.99558@gmail.com>
2026-07-10 23:52:20 +0530

    10th july

--- countinversion.cpp
+++ countinversion.cpp
@@ -3,50 +3,46 @@ using namespace std;
 int arr[]={4, 3, 6, 2, 1, 1};
 int main(){
     int n=sizeof(arr)/sizeof(arr[0]);
-    /*for(int i=1;i<n;i++){
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(arr[j]==i){
-                    cnt++;
-                }
-            }
-            if(cnt==0){
-                cout<<"Missing number is "<<i<<endl;
-            }
-            else if(cnt==2){
-                cout<<"Repeating number is "<<i<<endl;
-            }//bruete force solution time complexity is O(n^2) and space complexity is O(1)
-        }*/
-     /*int hash[n+1]={0};
-     for(int i=0;i<n;i++){
-         hash[arr[i]]++;
-     }
-     int repeating=-1,missing=-1;
-        for(int i=1;i<=n;i++){
-            if(hash[i]==2){
-                repeating=i;
-            }
-            else if(hash[i]==0){
-                missing=i;
-            }
-            if(repeating!=-1 && missing!=-1){
-                break;
-            }//better solution time complexity is O(n) and space complexity is O(n)
-    cout<<"Repeating number is "<<repeating<<endl;
-    cout<<"Missing number is "<<missing<<endl;*/ 
-    //optimal solution time complexity is O(n) and space complexity is O(1)
-    int sn=(n*(n+1))/2;
-    int s2n=(n*(n+1)*(2*n+1))/6;
-    int s=0,s2=0;
-    for(int i=0;i<n;i++){
-        s+=arr[i];
-        s2+=arr[i]*arr[i];  
-        if(arr[i]==arr[arr[i]-1]){
-            cout<<"Repeating number is "<<arr[i]<<endl;
+    int cnt=0;
+   /*for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                cnt++;
+            }       
         }
-        else{
-            swap(arr[i],arr[arr[i]-1]);
-            i--;
+    }
+    cout<<"Inversion count is "<<cnt<<endl;
+//brute force solution time complexity is O(n^2) and space complexity is O(1).*/
+
+//better solution using merge sort time complexity is O(nlogn) and space complexity is O(n).
+    int temp[n];
+    function<int(int,int)> mergeSort=[&](int l,int r)->int{
+        if(l>=r){
+            return 0;
         }
-}
+        int mid=(l+r)/2;
+        int inv=mergeSort(l,mid);
+        inv+=mergeSort(mid+1,r);
+        int i=l,j=mid+1,k=l;
+        while(i<=mid && j<=r){
+            if(arr[i]<=arr[j]){
+                temp[k++]=arr[i++];
+            }
+            else{
+                temp[k++]=arr[j++];
+                inv+=(mid-i+1);
+            }
+        }
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=r){
+            temp[k++]=arr[j++];
+        }
+        for(int i=l;i<=r;i++){
+            arr[i]=temp[i];
+        }
+        return inv;
+    };
+    cout<<"Inversion count is "<<mergeSort(0,n-1)<<endl;
 }
